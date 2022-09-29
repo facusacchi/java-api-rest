39082dc4271ca80b54238be910ac608fb2b12f29
facu sacchi <faq.ndo.n.s@gmail.com>
2022-09-29 20:06:34 -0300

    first commit

---
+++ src/main/java/com/javaapirest/JavaApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.javaapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiRestApplication.class, args);
+	}
+
+}
